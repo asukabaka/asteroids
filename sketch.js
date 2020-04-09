a0ea8edf9a33ad356835d9f2744e7e55600d5543
asukabaka <48495833+asukabaka@users.noreply.github.com>
2020-04-08 17:47:29 -0700

    asteroids

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var ship;
+var asteroids = [];
+
+
+function setup() {
+  createCanvas(windowWidth-100, windowHeight-100);
+  ship = new Ship();
+  asteroids.push(new Asteroid());
+
+}
+
+function draw() {
+ background(0);
+ ship.render();
+ ship.turn();
+ ship.update();
+ ship.edges();
+
+ for (var i = 0; i < asteroids.length; i++) {
+  asteroids[i].render();
+ }
+}
+
+function keyReleased() {
+  ship.setRotation(0);
+  ship.boosting(false);
+}
+
+function keyPressed(){
+  if (keyCode == RIGHT_ARROW) {
+    ship.setRotation(0.1);  
+  } else if (keyCode == LEFT_ARROW) {
+    ship.setRotation(-0.1); 
+  } else if (keyCode == UP_ARROW) {
+    ship.boosting(true);
+  }
+}
